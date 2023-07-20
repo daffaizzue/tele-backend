4ce9b840c821bd2af7cec27eddfcf1ad962bf457
daffaizzue <daffaizzue@gmail.com>
2023-07-20 20:03:30 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send({ message: "Ready" });
+});
+
+app.listen(port, () => console.log(`Listening app on port: ${port}`));
